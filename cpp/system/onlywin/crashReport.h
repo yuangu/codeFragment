215206d86e2350951bb28255f48ef43392502e32
yuangu <lifulinghan@aol.com>
2017-08-07 17:31:23 +0800

    上报系统加上日期

--- cpp/system/onlywin/crashReport.h
+++ cpp/system/onlywin/crashReport.h
@@ -1,13 +1,24 @@
 #include <windows.h>
 #include <Dbghelp.h>
-#include "util/utils.h"
+#include <direct.h>
+#include <time.h>
 #pragma auto_inline (off)
 #pragma comment( lib, "DbgHelp" )
 
-long   __stdcall  CrashCallBack(_EXCEPTION_POINTERS* pExInfo)
+long __stdcall  CrashCallBack(_EXCEPTION_POINTERS* pExInfo)
 {
-	 std::string p = Utils::getwd();
-	 p =  p + "/dumpfile.dmp";
+	 char buffer[_MAX_PATH] = {0x00};
+	 getcwd(buffer, _MAX_PATH);
+	 std::string p = buffer;
+	 std::cout << p << std::endl;
+
+	 //获取以时间为准 
+	 time_t lt = time(NULL);
+	 struct tm* t = localtime(&lt);
+	 memset(buffer, 0, _MAX_PATH);
+	 sprintf(buffer, "/dumpfile_%04d-%02d-%02d_%02d-%02d-%02d.dmp", 1900 + t->tm_year, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+
+	 p =  p + buffer;
      HANDLE hFile = ::CreateFileA(p.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
      if( hFile != INVALID_HANDLE_VALUE)
      {
